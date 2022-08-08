2c86405dd029e7da7076f750a54b7d6419519495
OneCrazyman <cjw7155@naver.com>
2022-08-08 18:27:15 +0900

    text

--- pybo/views.py
+++ pybo/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("안녕하세요 pybo hello")
+    return HttpResponse("<h1>정재훈 이새1키 왜 견적 맞추러 신창까지옴?</h1>")
