2101351eacc1772eb6bfb7c2b4ac53b8c7624b33
OneCrazyman <cjw7155@naver.com>
2022-08-21 22:46:26 +0900

    0821-1

--- pybo/views.py
+++ pybo/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
-
+# from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Question
 def index(request):
-    return HttpResponse("<h1>정재훈 이새1키 왜 견적 맞추러 신창까지옴?</h1>")
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
